c4c54b24867b5e674d53482b453d605d0156d1ac
Rolf Bjarne Kvinge <rolf@xamarin.com>
2019-02-06 10:47:53 +0100

    debug stuff.

--- mcs/class/test-helpers/NetworkHelpers.cs
+++ mcs/class/test-helpers/NetworkHelpers.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading;
 using System.Collections.Generic;
 
 namespace MonoTests.Helpers {
@@ -27,21 +28,36 @@ namespace MonoTests.Helpers {
 
 				var ep = new IPEndPoint (IPAddress.Loopback, testingPort);
 
-				lock (portsTable) {
-					if (portsTable.Contains (testingPort))
-						continue;
-
-					++counter;
-
-					try {
-						using (var socket = new Socket (ep.AddressFamily, SocketType.Stream, ProtocolType.Tcp)) {
-							socket.Bind (ep);
-							socket.Close ();
+				try {
+					lock (portsTable) {
+						Console.WriteLine ("[{0}] LocalEphemeralEndPoint locked", Thread.CurrentThread.ManagedThreadId);
+						if (portsTable.Contains (testingPort)) {
+							Console.WriteLine ("[{0}] LocalEphemeralEndPoint locked 1", Thread.CurrentThread.ManagedThreadId);
+							continue;
 						}
 
-						portsTable.Add (testingPort);
-						return ep;
-					} catch (SocketException) { }
+						++counter;
+						Console.WriteLine ("[{0}] LocalEphemeralEndPoint locked 2 {1}", Thread.CurrentThread.ManagedThreadId, counter);
+
+						try {
+							using (var socket = new Socket (ep.AddressFamily, SocketType.Stream, ProtocolType.Tcp)) {
+								Console.WriteLine ("[{0}] LocalEphemeralEndPoint locked 3", Thread.CurrentThread.ManagedThreadId);
+								socket.Bind (ep);
+								Console.WriteLine ("[{0}] LocalEphemeralEndPoint locked 4", Thread.CurrentThread.ManagedThreadId);
+								socket.Close ();
+								Console.WriteLine ("[{0}] LocalEphemeralEndPoint locked 5", Thread.CurrentThread.ManagedThreadId);
+							}
+
+							portsTable.Add (testingPort);
+							Console.WriteLine ("[{0}] LocalEphemeralEndPoint found port: {1}", Thread.CurrentThread.ManagedThreadId, testingPort);
+							return ep;
+						} catch (SocketException sex) {
+							Console.WriteLine ("[{0}] LocalEphemeralEndPoint exc: {1}", Thread.CurrentThread.ManagedThreadId, sex.Message);
+						}
+						Console.WriteLine ("[{0}] LocalEphemeralEndPoint locked 6", Thread.CurrentThread.ManagedThreadId);
+					}
+				} finally {
+					Console.WriteLine ("[{0}] LocalEphemeralEndPoint unlocked", Thread.CurrentThread.ManagedThreadId);
 				}
 			}
 
